c885ab84fc812ce075448bdf5746743ff4efeb70
Hussnian-Shahid <hs3277860303@gmail.com>
2025-04-24 12:40:15 +0500

    header + partner

---
+++ src/Component/Partner.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Partner = () => {
+  return (
+    <div className='md:px-16.5 py-17 ' >
+      <div className='text-center text-[22px] font-urbanist' >Collaborated with Trusted Worldwide Partners</div>
+      <div className='flex justify-center items-center gap-35 py-5 ' >
+        <div>
+          <img className='w-35' src="/partner1.png" alt="" />
+        </div>
+        <div>
+          <img className='w-35' src="/partner2.png" alt="" />
+        </div>
+        <div>
+          <img className='w-35' src="/partner3.png" alt="" />
+        </div>
+        <div>
+          <img className='w-35' src="/partner4.png" alt="" />
+        </div>
+        <div>
+          <img className='w-35' src="/partner5.png" alt="" />
+        </div>
+        <div>
+          <img className='w-35' src="/partner6.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Partner
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './Component/Navbar'
+import Header from './Component/Header'
+import Partner from './Component/Partner'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
+    <Header/>
+    <Partner/>
     </>
   )
 }
